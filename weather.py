c8613febf463b9678494c3529acc4365e03f4e8c
brad90four <42116429+brad90four@users.noreply.github.com>
2022-04-13 10:23:07 -0400

    extend and customize cli with forecast

--- weather.py
+++ weather.py
@@ -1,4 +1,5 @@
 import argparse
+import datetime
 import json
 import os
 import re
@@ -7,11 +8,14 @@ from pathlib import Path
 from urllib import error, parse, request
 
 from dotenv import load_dotenv
+from loguru import logger
+
 
 root_dir = Path(__file__).parent
 load_dotenv(root_dir.joinpath(".env"))
 API_KEY = os.environ.get("API_KEY")
 BASE_WEATHER_API_URL = "http://api.openweathermap.org/data/2.5/weather"
+FORECAST_API_URL = "http://api.openweathermap.org/data/2.5/forecast"
 PADDING = 20
 THUNDERSTORM = range(200, 300)
 DRIZZLE = range(300, 400)
@@ -31,12 +35,29 @@ def read_user_cli_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser(
         description="gets weather and temperature info for a city"
     )
-    parser.add_argument("city", nargs="+", type=str, help="enter the city name")
+    parser.add_argument(
+        "city",
+        nargs="+",
+        type=str,
+        help="enter the city name"
+    )
     parser.add_argument(
         "-v",
         "--verbose",
         action="store_true",
         help="display additional output for the query",
+    )
+    parser.add_argument(
+        "-f",
+        "--forecast",
+        action="store_true",
+        help="get the forecasted weather for the next 5 days",
+    )
+    parser.add_argument(
+        "-d",
+        "--debug",
+        action="store_true",
+        help="run the script in debug mode for more detailed information",
     )
     return parser.parse_args()
 
@@ -70,36 +91,86 @@ def _select_weather_display_emoji(weather_id: int) -> str:
     return display_emoji
 
 
-def display_weather_data(data: dict, verbose: bool = False) -> None:
+def display_weather_data(data: dict, verbose: bool = False, forecast: bool = False) -> None:
     """Displays the weather data from the API response.
 
     Args:
         data (dict): JSON response from the API.
+        verbose (bool): Display additional output for the query.
+        forecast (bool): Display the forecasted weather for the next 5 days.
     """
-    city = data["name"]
-    temp = data["main"]["temp"]
-    weather_id = data["weather"][0]["id"]
-    weather_emoji = _select_weather_display_emoji(weather_id)
-    weather_type = data["weather"][0]["description"]
-    print(
-        f"{city:<{PADDING}} {weather_emoji} {weather_type.capitalize():<{PADDING}}{temp}°F"
-    )
-    if verbose:
-        humidity = data["main"]["humidity"]
-        wind_speed = data["wind"]["speed"]
-        min_temp = data["main"]["temp_min"]
-        max_temp = data["main"]["temp_max"]
+    if forecast:
+        city = data["city"]["name"]
+        forecast_data = {}
+        for datestamp in data["list"]:
+            local_time = datetime.datetime.fromtimestamp(datestamp["dt"]).strftime("%Y-%m-%d %H:%M")
+            forecast_data[local_time] = {
+                "temp": datestamp["main"]["temp"],
+                "weather_type": datestamp["weather"][0]["description"],
+                "weather_id": datestamp["weather"][0]["id"],
+                "humidity": datestamp["main"]["humidity"],
+                "wind_speed": datestamp["wind"]["speed"]
+            }
+            try:
+                forecast_data[local_time]["rain"] = round(datestamp["rain"]["3h"] * 0.0393701, 2)
+            except KeyError:
+                pass
+
+    else:
+        city = data["name"]
+        temp = data["main"]["temp"]
+        weather_id = data["weather"][0]["id"]
+        weather_emoji = _select_weather_display_emoji(weather_id)
+        weather_type = data["weather"][0]["description"]
         print(
-            f"Max:{max_temp}°F | Min:{min_temp}°F | "
-            f"Humidity:{humidity}% | Wind:{wind_speed}mph"
+            f"{city:<{PADDING}} {weather_emoji} {weather_type.capitalize():<{PADDING}}{temp}°F"
         )
+    if forecast:
+        for datestamp in forecast_data:
+            #logger.debug(f"{datestamp = }")
+            temp = forecast_data[datestamp]["temp"]
+            weather_type = forecast_data[datestamp]["weather_type"]
+            weather_emoji = _select_weather_display_emoji(forecast_data[datestamp]["weather_id"])
+            if verbose:
+                humidity = forecast_data[datestamp]["humidity"]
+                wind_speed = forecast_data[datestamp]["wind_speed"]
+                try:
+                    rainfall = forecast_data[datestamp]["rain"]
+                except KeyError:
+                    rainfall = 0
+                print(
+                    f"{datestamp:<{PADDING}} {weather_emoji} "
+                    f"{weather_type.capitalize():<{PADDING}}{temp}°F "
+                    f"{rainfall}in rain, {humidity}% humidity, {wind_speed} mph"
+                )
+            else:
+                print(
+                    f"{datestamp:<{PADDING}} {weather_emoji} {weather_type.capitalize():<{PADDING}}{temp}°F"
+                )
+    if verbose:
+        if forecast:
+            pass
+        else:
+            humidity = data["main"]["humidity"]
+            wind_speed = data["wind"]["speed"]
+            min_temp = data["main"]["temp_min"]
+            max_temp = data["main"]["temp_max"]
+            try:
+                rainfall = round(data["rain"]["3h"] * 0.0393701, 2)
+            except KeyError:
+                rainfall = 0
+            print(
+                f"Max:{max_temp}°F | Min:{min_temp}°F | "
+                f"{rainfall}in rain | {humidity}% humidity | {wind_speed} mph"
+            )
 
 
-def build_weather_query(city_input: list[str]) -> str:
+def build_weather_query(city_input: list[str], forecast: bool = False) -> str:
     """Builds the url for an API request to OpenWeather's API.
 
     Args:
         city_input (List[str]): Name of a city from user input.
+        forecast (bool): Display the forecasted weather for the next 5 days.
 
     Returns:
         str: URL formatted for a call to the OpenWeather's city name endpoint.
@@ -107,18 +178,26 @@ def build_weather_query(city_input: list[str]) -> str:
     city_name = " ".join(city_input)
     url_encoded_city_name = parse.quote_plus(city_name)
     units = "imperial"
-    url = (
-        f"{BASE_WEATHER_API_URL}?q={url_encoded_city_name}"
-        f"&appid={API_KEY}&units={units}"
-    )
+    if forecast:
+        count = 8  # number of 3-hour blocks to use in API call
+        url = (
+            f"{FORECAST_API_URL}?q={url_encoded_city_name}"
+            f"&appid={API_KEY}&units={units}&cnt={count}"
+        )
+    else:
+        url = (
+            f"{BASE_WEATHER_API_URL}?q={url_encoded_city_name}"
+            f"&appid={API_KEY}&units={units}"
+        )
     return url
 
 
-def get_weather_data(query_url: str) -> dict:
+def get_weather_data(query_url: str, debug: bool = False) -> dict:
     """Makes an API request to a given URL and returns the response.
 
     Args:
         query_url (str): URL formatted for a call to the OpenWeather's city name endpoint.
+        debug (bool): Display additional output for the query.
 
     Returns:
         dict: JSON response from the API.
@@ -138,6 +217,8 @@ def get_weather_data(query_url: str) -> dict:
 
     data = response.read()
     try:
+        if debug:
+            logger.debug(json.loads(data))
         return json.loads(data)
     except json.JSONDecodeError:
         sys.exit("JSON decode error")
@@ -146,6 +227,8 @@ def get_weather_data(query_url: str) -> dict:
 if __name__ == "__main__":
     user_args = read_user_cli_args()
     verbosity = True if user_args.verbose else False
-    query_url = build_weather_query(user_args.city)
-    weather_data = get_weather_data(query_url)
-    display_weather_data(weather_data, verbosity)
+    forecast = True if user_args.forecast else False
+    debug = True if user_args.debug else False
+    query_url = build_weather_query(user_args.city, forecast)
+    weather_data = get_weather_data(query_url, debug)
+    display_weather_data(weather_data, verbosity, forecast)
